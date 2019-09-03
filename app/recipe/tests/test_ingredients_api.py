c456686e7d5646330a83922a8784357bf30f1056
Kevin Rodríguez <kyrodriguez97@gmail.com>
2019-09-03 10:31:06 -0500

    Add list of ingredients

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,61 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+class PublicIngredientsApiTests(TestCase):
+
+  def setUp(self):
+    self.client = APIClient(self)
+
+  def test_login_required(self):
+    response = self.client.get(INGREDIENTS_URL)
+
+    self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateIngredientsApiTest(TestCase):
+
+  def setUp(self):
+    self.client = APIClient()
+    self.user = get_user_model().objects.create_user(
+      'email@email.com',
+      '1qazxsw2'
+    )
+
+    self.client.force_authenticate(self.user)
+
+  def test_retrieve_ingredient_list(self):
+    Ingredient.objects.create(user=self.user, name='Kale')
+    Ingredient.objects.create(user=self.user, name='Salt')
+
+    response = self.client.get(INGREDIENTS_URL)
+
+    ingredients = Ingredient.objects.all().order_by('-name')
+    serializer = IngredientSerializer(ingredients, many=True)
+
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(response.data, serializer.data)
+
+  def test_ingredients_limited_to_user(self):
+    user_two = get_user_model().objects.create_user(
+      'email_two@email.com',
+      '1qazxsw2'
+    )
+
+    Ingredient.objects.create(user=user_two, name='Joe')
+    ingredient = Ingredient.objects.create(user=self.user, name='Doe')
+
+    response = self.client.get(INGREDIENTS_URL)
+
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(len(response.data), 1)
+    self.assertEqual(response.data[0]['name'], ingredient.name)
+
