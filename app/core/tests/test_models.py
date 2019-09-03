4b52ef2075d9fd06198b36e59676d0307177197a
Kevin Rodríguez <kyrodriguez97@gmail.com>
2019-09-03 11:58:07 -0500

    Add Recipe Model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,3 +53,11 @@ class ModelTests(TestCase):
     )
 
     self.assertEqual(str(ingredient), ingredient.name)
+
+  def test_recipe_str(self):
+    recipe = models.Recipe.objects.create(
+      user = sample_user(),
+      title = 'Steak',
+      time_in_minutes = 5,
+      price = 5.00
+    )
