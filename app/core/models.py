67b147ae80de8886c15c3c09cc81f3cbf5d90817
Kevin Rodríguez <kyrodriguez97@gmail.com>
2019-09-03 15:17:50 -0500

    Add list recipe feature

--- app/core/models.py
+++ app/core/models.py
@@ -59,7 +59,7 @@ class Recipe(models.Model):
   time_in_minutes = models.IntegerField()
   price = models.DecimalField(max_digits=5, decimal_places=2)
   link = models.CharField(max_length=255, blank=True)
-  ingredientes = models.ManyToManyField('Ingredient')
+  ingredients = models.ManyToManyField('Ingredient')
   tags = models.ManyToManyField('Tag')
 
   def __str__(self):
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 class TagSerializer(serializers.ModelSerializer):
 
@@ -15,5 +15,31 @@ class IngredientSerializer(serializers.ModelSerializer):
   class Meta:
     model = Ingredient
     fields = ('id', 'name')
+    read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+  ingredients = serializers.PrimaryKeyRelatedField(
+    many= True,
+    queryset = Ingredient.objects.all()
+  )
+
+  tags = serializers.PrimaryKeyRelatedField(
+      many=True,
+      queryset=Tag.objects.all()
+  )
+
+  class Meta:
+    model = Recipe
+    fields = (
+      'id', 
+      'title', 
+      'ingredients', 
+      'tags', 
+      'time_in_minutes', 
+      'price', 
+      'link',
+    )
+
     read_only_fields = ('id',)
 
