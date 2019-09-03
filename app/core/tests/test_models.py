a41d696df49cbc68beaea144f003dff080a9404c
Kevin Rodríguez <kyrodriguez97@gmail.com>
2019-09-03 18:49:02 -0500

    Add image field

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from unittest.mock import patch
+
 from core import models
 
 def sample_user(email='email@email.com', password='1qazxsw2'):
@@ -61,3 +63,13 @@ class ModelTests(TestCase):
       time_in_minutes = 5,
       price = 5.00
     )
+
+  @patch('uuid.uuid4')
+  def test_recipe_file_name_uuid(self, mock_uuid):
+    uuid = 'test-uuid'
+    mock_uuid.return_value = uuid
+    file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+    exp_path = f'uploads/recipe/{uuid}.jpg'
+    self.assertEqual(file_path, exp_path)
+
