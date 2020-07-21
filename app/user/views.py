ccdbe9f7d2ca7518dded89fc7889acb74ec0d1a0
Kevin Rodríguez <kevin.rodriguez@greensill.com>
2020-07-21 09:54:43 -0500

    pre-commit configuration

--- app/user/views.py
+++ app/user/views.py
@@ -10,6 +10,8 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class CreateTokenView(ObtainAuthToken):
+    """Create Token"""
+
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
